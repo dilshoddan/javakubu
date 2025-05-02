44d0a59aac4e0404b3d172b0f3ff6dcbf4e906b0
Dilshod Abdullaev <dilshod.abdullaev@Dilshods-MacBook-Pro.local>
2025-05-02 17:11:49 -0500

    Add Angular Material and NgBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { QuestionComponent } from './question/question.component';
+
+
+const MAT_MODULES = [MatFormFieldModule, MatInputModule, MatSelectModule, MatCheckboxModule, MatDividerModule];
+const NGB_MODULES = [NgbModule];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MAT_MODULES,
+    NGB_MODULES
+
   ],
   providers: [],
   bootstrap: [AppComponent]
